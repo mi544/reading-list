ef5797748b26b3f983ff38935cc2d94e101f5208
Maksim Verkhoturov <sd32@protonmail.com>
2021-04-16 16:46:40 -0600

    b:adds reg&login routes, installs passport, configures auth

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Register
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'min:1', 'max:50'],
+            'username' => ['required', 'string', 'min:3', 'max:50', 'unique:users'],
+            'password' => ['required', 'string', 'min:5', 'max:50'],
+        ]);
+
+        if ($validator->fails()) {
+            $response_data = [
+                'status' => 'validation_error',
+                'errors' => $validator->errors(),
+            ];
+
+            return response()->json($response_data, 400);
+        }
+
+        $credentials = request()->only('name', 'username', 'password');
+
+        User::create([
+            'name' => $credentials['name'],
+            'username' => $credentials['username'],
+            'password' => Hash::make($credentials['password']),
+        ]);
+
+        $auth_attempt = Auth::attempt($credentials);
+
+        if ($auth_attempt) {
+            $access_token = Auth::user()->createToken('authToken')->accessToken;
+            
+            return response()->json([
+                'user' => Auth::user(),
+                'access_token' => $access_token
+            ]);
+        }
+
+        return response()->json(['status' => 'auth_error', 500]);
+    }
+
+    /**
+     * Login
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => ['required', 'string', 'min:3', 'max:50'],
+            'password' => ['required', 'string', 'min:5', 'max:50'],
+        ]);
+
+        if ($validator->fails()) {
+            $response_data = [
+                'status' => 'validation_error',
+                'errors' => $validator->errors(),
+            ];
+
+            return response()->json($response_data, 400);
+        }
+
+        $credentials = request()->only('username', 'password');
+
+        $auth_attempt = Auth::attempt($credentials);
+
+        if ($auth_attempt) {
+            $access_token = Auth::user()->createToken('authToken')->accessToken;
+            
+            return response()->json([
+                'access_token' => $access_token
+            ]);
+        }
+
+        return response()->json(['status' => 'auth_error', 'error'=>'Invalid credentials'] ,401);
+    }
+
+    
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +18,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'username',
         'password',
     ];
 
@@ -38,6 +38,5 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
