fd115d2089bb57dcdc3971c2bc5a29b1d1766ca1
Maksim Verkhoturov <sd32@protonmail.com>
2021-04-17 11:14:19 -0600

    b:adds book model/migration

---
+++ backend/database/migrations/2021_04_16_235145_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('g_book_id')->unique();
+            $table->string('title');
+            $table->string('author');
+            $table->boolean('finished')->default(false);
+            $table->timestamp('finished_at')->nullable();
+            $table->unsignedInteger('order');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
