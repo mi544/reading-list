5964ef5d6d19d98c56fc21e9927819d72c6f2d95
Maksim Verkhoturov <sd32@protonmail.com>
2021-04-17 18:53:10 -0600

    f:adds vuex store (auth & error modules)

---
+++ frontend/src/store/auth.js
@@ -0,0 +1,15 @@
+const state = () => ({
+  isAuthenticated: null,
+  username: null,
+  name: null,
+})
+const mutations = {}
+const actions = {}
+const getters = {}
+
+export const authModule = {
+  state,
+  mutations,
+  actions,
+  getters,
+}
---
+++ frontend/src/store/error.js
@@ -0,0 +1,14 @@
+const state = () => ({
+  error: null,
+  errorMessage: null,
+})
+const mutations = {}
+const actions = {}
+const getters = {}
+
+export const errorModule = {
+  state,
+  mutations,
+  actions,
+  getters,
+}
