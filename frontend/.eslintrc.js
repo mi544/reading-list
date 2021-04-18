9bffa7104751ee9bed983c5e78f85b4bc5bfb88e
Maksim Verkhoturov <sd32@protonmail.com>
2021-04-17 23:39:53 -0600

    f:adds html title changing functionality

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
-  rules: {}
+  rules: {},
 }
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,21 +11,67 @@ import Book from '../pages/Book.vue'
 import NotFound from '../pages/_.vue'
 
 const routes = [
-  { path: '/', component: Index },
-  { path: '/about', component: About },
-  { path: '/contact', component: Contact },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
-  { path: '/dashboard', component: Dashboard },
-  { path: '/reading-list', component: ReadingList },
-  { path: '/books', component: Books },
-  { path: '/book/:gbook_id', component: Book },
-  { path: '/:pathMatch(.*)*', component: NotFound },
+  {
+    path: '/',
+    component: Index,
+    meta: { title: 'Book Store' },
+  },
+  {
+    path: '/about',
+    component: About,
+    meta: { title: 'About - Book Store' },
+  },
+  {
+    path: '/contact',
+    component: Contact,
+    meta: { title: 'Contact - Book Store' },
+  },
+  {
+    path: '/login',
+    component: Login,
+    meta: {
+      title: 'Login - Book Store',
+    },
+  },
+  {
+    path: '/register',
+    component: Register,
+    meta: { title: 'Register - Book Store' },
+  },
+  {
+    path: '/dashboard',
+    component: Dashboard,
+    meta: { title: 'Dashboard - Book Store' },
+  },
+  {
+    path: '/reading-list',
+    component: ReadingList,
+    meta: { title: 'Reading List - Book Store' },
+  },
+  {
+    path: '/books',
+    component: Books,
+    meta: { title: 'Books - Book Store' },
+  },
+  {
+    path: '/book/:gbook_id',
+    component: Book,
+    meta: { title: 'Book - Book Store' },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFound,
+    meta: { title: '404 Not Found - Book Store' },
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.afterEach((to) => {
+  window.document.title = to.meta.title
 })
 
 export default router
