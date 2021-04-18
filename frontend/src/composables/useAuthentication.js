f30aed9e72c98995fe3609e48d81478619348bb2
Maksim Verkhoturov <sd32@protonmail.com>
2021-04-18 13:25:53 -0600

    f:refactors composable to act in a more Vuex-like manner (statete persistence)

--- frontend/src/composables/useAuthentication.js
+++ frontend/src/composables/useAuthentication.js
@@ -1,73 +1,75 @@
 import { ref } from 'vue'
 import { getAccountInfo } from '@/serviceClients/authClient.js'
 
-export default function useAuthentication() {
-  const isAuthenticated = ref(null)
-  const name = ref(null)
-  const username = ref(null)
-  const token = ref(null)
+const isAuthenticated = ref(null)
+const name = ref(null)
+const username = ref(null)
+const token = ref(null)
 
-  const updateUserData = (userData) => {
-    isAuthenticated.value = true
-    window.localStorage.setItem('isAuthenticated', JSON.stringify(true))
-    name.value = userData.name
-    username.value = userData.username
-    token.value = userData.token
-    window.localStorage.setItem('userData', JSON.stringify(userData))
+const updateUserData = (userData) => {
+  isAuthenticated.value = true
+  window.localStorage.setItem('isAuthenticated', JSON.stringify(true))
+  name.value = userData.name
+  username.value = userData.username
+  token.value = userData.token
+  window.localStorage.setItem('userData', JSON.stringify(userData))
+}
+
+const resetAuth = () => {
+  isAuthenticated.value = false
+  window.localStorage.setItem('isAuthenticated', JSON.stringify(false))
+  name.value = null
+  username.value = null
+  token.value = null
+  window.localStorage.removeItem('userData')
+}
+
+const loadDataFromStorage = () => {
+  // check if isAuthenticated exists
+  const isAuthenticatedStore = window.localStorage.getItem('isAuthenticated')
+  if (isAuthenticatedStore === null || !JSON.parse(isAuthenticatedStore)) {
+    // break out if isAuthenticated is false or doesn't exist
+    return false
   }
 
-  const resetAuth = () => {
-    isAuthenticated.value = false
-    window.localStorage.setItem('isAuthenticated', JSON.stringify(true))
-    name.value = null
-    username.value = null
-    token.value = null
-    window.localStorage.removeItem('userData')
+  const userData = JSON.parse(window.localStorage.getItem('userData'))
+  name.value = userData.name
+  username.value = userData.username
+  token.value = userData.token
+  isAuthenticated.value = true
+
+  console.log('made it past, isAuthenticated', isAuthenticated.value)
+
+  return true
+}
+
+const checkIsTokenActive = async () => {
+  // check if isAuthenticated exists
+  const isAuthenticated = window.localStorage.getItem('isAuthenticated')
+  if (isAuthenticated === null || !JSON.parse(isAuthenticated)) {
+    // break out if isAuthenticated is false or doesn't exist
+    return false
   }
 
-  const loadDataFromStorage = () => {
-    // check if isAuthenticated exists
-    const isAuthenticated = window.localStorage.getItem('isAuthenticated')
-    if (isAuthenticated === null || !JSON.parse(isAuthenticated)) {
-      // break out if isAuthenticated is false or doesn't exist
-      return false
-    }
-
-    const userData = JSON.parse(window.localStorage.getItem('userData'))
-    name.value = userData.name
-    username.value = userData.username
-    token.value = userData.token
+  const { token } = JSON.parse(window.localStorage.getItem('userData'))
+  try {
+    // trying to get account info - will fail if token is invalid
+    await getAccountInfo(token)
     return true
+  } catch (err) {
+    // in case of an error reset the authenticated state and userData
+    resetAuth()
+    return false
   }
+}
 
-  const checkIsTokenActive = async () => {
-    // check if isAuthenticated exists
-    const isAuthenticated = window.localStorage.getItem('isAuthenticated')
-    if (isAuthenticated === null || !JSON.parse(isAuthenticated)) {
-      // break out if isAuthenticated is false or doesn't exist
-      return false
-    }
-
-    const { token } = JSON.parse(window.localStorage.getItem('userData'))
-    try {
-      // trying to get account info - will fail if token is invalid
-      await getAccountInfo(token)
-      return true
-    } catch (err) {
-      // in case of an error reset the authenticated state and userData
-      resetAuth()
-      return false
-    }
-  }
-
-  return {
-    isAuthenticated,
-    name,
-    username,
-    token,
-    updateUserData,
-    resetAuth,
-    loadDataFromStorage,
-    checkIsTokenActive,
-  }
+export {
+  isAuthenticated,
+  name,
+  username,
+  token,
+  updateUserData,
+  resetAuth,
+  loadDataFromStorage,
+  checkIsTokenActive,
 }
