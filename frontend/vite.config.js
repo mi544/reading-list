6e4e9acc742019bc82bf3afa5ef51d418c03d332
Maksim Verkhoturov <sd32@protonmail.com>
2021-04-17 21:38:52 -0600

    f:adds import aliases

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@C': path.resolve(__dirname, 'src/components'),
+    },
+  },
+  extensions: ['.js', '.vue', '.json'],
 })
