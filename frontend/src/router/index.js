b0afb20a8ceb664759766f4aaa9435c4f87922f0
Maksim Verkhoturov <sd32@protonmail.com>
2021-04-18 11:19:50 -0600

    f:adds route guard for protected routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import useAuthentication from '@/composables/useAuthentication.js'
 import Index from '../pages/Index.vue'
 import About from '../pages/About.vue'
 import Contact from '../pages/Contact.vue'
@@ -14,60 +15,94 @@ const routes = [
   {
     path: '/',
     component: Index,
-    meta: { title: 'Book Store' },
+    meta: {
+      title: 'Book Store',
+      public: true,
+    },
   },
   {
     path: '/about',
     component: About,
-    meta: { title: 'About - Book Store' },
+    meta: {
+      title: 'About - Book Store',
+      public: true,
+    },
   },
   {
     path: '/contact',
     component: Contact,
-    meta: { title: 'Contact - Book Store' },
+    meta: {
+      title: 'Contact - Book Store',
+      public: true,
+    },
   },
   {
     path: '/login',
     component: Login,
     meta: {
       title: 'Login - Book Store',
+      public: true,
     },
   },
   {
     path: '/register',
     component: Register,
-    meta: { title: 'Register - Book Store' },
+    meta: {
+      title: 'Register - Book Store',
+      public: true,
+    },
   },
   {
     path: '/dashboard',
     component: Dashboard,
-    meta: { title: 'Dashboard - Book Store' },
+    meta: {
+      title: 'Dashboard - Book Store',
+      public: false,
+    },
   },
   {
     path: '/reading-list',
     component: ReadingList,
-    meta: { title: 'Reading List - Book Store' },
+    meta: {
+      title: 'Reading List - Book Store',
+      public: false,
+    },
   },
   {
     path: '/books',
     component: Books,
-    meta: { title: 'Books - Book Store' },
+    meta: {
+      title: 'Books - Book Store',
+      public: true,
+    },
   },
   {
     path: '/book/:gbook_id',
     component: Book,
-    meta: { title: 'Book - Book Store' },
+    meta: {
+      title: 'Book - Book Store',
+      public: true,
+    },
   },
   {
     path: '/:pathMatch(.*)*',
     component: NotFound,
-    meta: { title: '404 Not Found - Book Store' },
+    meta: {
+      title: '404 Not Found - Book Store',
+      public: true,
+    },
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to) => {
+  const { isAuthenticated } = useAuthentication()
+  if (!to.meta.public && !isAuthenticated.value) return '/login'
+  return true
 })
 
 router.afterEach((to) => {
