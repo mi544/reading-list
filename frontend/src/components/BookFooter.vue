39fdf174654bcb9e5f0c1486e57b799ea21eb02f
Maksim Verkhoturov <sd32@protonmail.com>
2021-04-24 21:04:33 -0600

    f:adds mobile responsiveness to header and footer, drop down menu

--- frontend/src/components/BookFooter.vue
+++ frontend/src/components/BookFooter.vue
@@ -2,10 +2,14 @@
   <footer class="h-12 bg-primary-100 border-black border-t text-xl">
     <nav class="min-h-full">
       <ul class="mt-2 flex justify-between items-stretch">
-        <div class="flex-grow w-8/12 flex justify-around items-center">
+        <div
+          class="flex-grow w-5/12 sm:w-7/12 md:w-8/12 flex justify-around items-center"
+        >
           <link-list-item :to="{ name: 'Love' }">&lt;3</link-list-item>
         </div>
-        <div class="flex-grow w-4/12 flex justify-around items-center">
+        <div
+          class="flex-grow w-7/12 sm:w-5/12 md:w-4/12 flex justify-around items-center"
+        >
           <link-list-item :to="{ name: 'About' }">About</link-list-item>
           <link-list-item :to="{ name: 'Contact' }">Contact</link-list-item>
         </div>
--- frontend/src/components/BookHeader.vue
+++ frontend/src/components/BookHeader.vue
@@ -2,7 +2,7 @@
   <header
     class="h-32 bg-primary-200 flex justify-between items-center border-black border-b-2 rounded-b-md shadow-md"
   >
-    <div class="h-24 w-3/12">
+    <div class="h-24 md:w-5/12 lg:w-4/12">
       <router-link
         :to="{ name: 'Index' }"
         class="h-full w-full p-2 flex items-center shadow-lg"
@@ -10,20 +10,48 @@
         <img
           src="@/assets/images/open-book.svg"
           alt="Open Book Icon"
-          class="h-full pl-6"
+          class="h-full pl-1 sm:pl-6"
         />
-        <h1 class="pl-6 text-3xl">Book Store</h1>
+        <h1 class="pl-1 sm:pl-6 text-3xl text-center">Book Store</h1>
       </router-link>
     </div>
-    <div class="w-4/12"></div>
-    <nav class="w-5/12 text-xl">
-      <ul class="flex justify-evenly items-center">
-        <link-list-item :to="{ name: 'Books' }">Explore Books</link-list-item>
-        <div class="w-1/12"></div>
-        <link-list-item v-if="isAuthenticated" :to="{ name: 'Dashboard' }">
+    <div class="hidden md:block md:w-2/12 lg:w-3/12"></div>
+    <button class="md:hidden h-24 w-24 mr-4 sm:mr-10" @click="onMenuToggle">
+      <div class="h-6 w-12 m-auto flex flex-col justify-between">
+        <div class="border-t-4 border-black"></div>
+        <div class="w-10 mx-auto border-t-4 border-black"></div>
+        <div class="border-t-4 border-black"></div>
+      </div>
+    </button>
+    <nav
+      :class="[
+        isMobile
+          ? isMenuOpen
+            ? 'absolute h-64 w-64 top-32 right-2'
+            : 'hidden'
+          : 'md:w-5/12',
+      ]"
+    >
+      <ul
+        :class="[
+          'h-full md:h-auto flex flex-col md:flex-row justify-evenly items-center text-2xl md:text-xl',
+          'bg-info-100 md:bg-transparent rounded-xl md:rounded-none',
+        ]"
+      >
+        <link-list-item :to="{ name: 'Books' }" @click="onLinkClick">
+          Explore Books
+        </link-list-item>
+        <div class="hidden md:block md:w-1/12"></div>
+        <link-list-item
+          v-if="isAuthenticated"
+          :to="{ name: 'Dashboard' }"
+          @click="onLinkClick"
+        >
           Dashboard
         </link-list-item>
-        <link-list-item v-else :to="{ name: 'Login' }">Login</link-list-item>
+        <link-list-item v-else :to="{ name: 'Login' }" @click="onLinkClick"
+          >Login</link-list-item
+        >
         <book-button v-if="isAuthenticated" @click="logout" small red>
           Logout
         </book-button>
@@ -33,6 +61,7 @@
 </template>
 
 <script>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { useRouter } from 'vue-router'
 import {
   isAuthenticated,
@@ -49,7 +78,37 @@ export default {
   setup() {
     const router = useRouter()
 
+    const isMenuOpen = ref(false)
+    const isMobile = ref(false)
+
+    const onResize = () => {
+      const screenWidth = window.screen.availWidth
+      isMobile.value = screenWidth < 768 ? true : false
+      // will set isMenuOpen value to false
+      // every time the screen is resized to be wider than 768px
+      // to prevent having opened menu when resizing to small screen back
+      isMenuOpen.value = screenWidth >= 768 ? false : isMenuOpen.value
+    }
+
+    onMounted(() => {
+      onResize()
+      window.addEventListener('resize', onResize)
+    })
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('resize', onResize)
+    })
+
+    const onMenuToggle = () => {
+      isMenuOpen.value = !isMenuOpen.value
+    }
+
+    const onLinkClick = () => {
+      isMenuOpen.value = false
+    }
+
     const logout = async () => {
+      onLinkClick()
       try {
         await logoutAccount(token.value)
       } catch (err) {
@@ -60,7 +119,14 @@ export default {
       }
     }
 
-    return { isAuthenticated, logout }
+    return {
+      isAuthenticated,
+      logout,
+      isMenuOpen,
+      isMobile,
+      onMenuToggle,
+      onLinkClick,
+    }
   },
 }
 </script>
